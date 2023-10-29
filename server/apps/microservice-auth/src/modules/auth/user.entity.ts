91595b65a9afb89a8d2f72e1121d332fea19f13a
daspete <daspetemail@gmail.com>
2023-10-29 10:46:38 +0100

    feat: updated workspace, fixed format error

--- server/apps/microservice-auth/src/modules/auth/user.entity.ts
+++ server/apps/microservice-auth/src/modules/auth/user.entity.ts
@@ -1,39 +1,39 @@
 import {
-  Field,
-  HideField,
-  ID,
-  ObjectType,
-  registerEnumType,
+    Field,
+    HideField,
+    ID,
+    ObjectType,
+    registerEnumType,
 } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export enum UserStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE',
 }
 
 registerEnumType(UserStatus, {
-  name: 'UserStatus',
+    name: 'UserStatus',
 });
 
 @ObjectType()
 @Schema({ timestamps: true, collection: 'users' })
 export class User {
-  @Field(() => ID)
-  id: string;
+    @Field(() => ID)
+    id: string;
 
-  @Field()
-  @Prop({ required: true, unique: true })
-  email: string;
+    @Field()
+    @Prop({ required: true, unique: true })
+    email: string;
 
-  @Prop({ required: true })
-  @HideField()
-  password: string;
+    @Prop({ required: true })
+    @HideField()
+    password: string;
 
-  @Field(() => UserStatus)
-  @Prop({ enum: UserStatus, default: UserStatus.ACTIVE })
-  status: UserStatus;
+    @Field(() => UserStatus)
+    @Prop({ enum: UserStatus, default: UserStatus.ACTIVE })
+    status: UserStatus;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
